fe6f111ab9977a8f1c3ee353be8877212a47d5f4
Csaba Marosi <marosicsaba91@gmail.com>
2022-12-14 21:44:10 +0100

    small fix

--- Script/Variables/Editor/VariableTreeDrawer.cs
+++ Script/Variables/Editor/VariableTreeDrawer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -327,3 +329,4 @@ static class VariableTreeDrawer
     }
 }
 }
+#endif
